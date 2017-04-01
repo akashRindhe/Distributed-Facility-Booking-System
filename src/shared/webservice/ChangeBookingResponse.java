dcd8d1541f71554a4f1646ea9c18e650fbeaf6ed
Shantanu Gupta <gupta.shantanu95@gmail.com>
2017-04-01 18:57:13 +0800

    remove unnecessary code

--- src/shared/webservice/ChangeBookingResponse.java
+++ src/shared/webservice/ChangeBookingResponse.java
@@ -4,15 +4,6 @@ import shared.Marshallable;
 
 public class ChangeBookingResponse implements Marshallable {
 	private String acknowledgement;
-	int success;
-
-	public int getSuccess() {
-		return success;
-	}
-
-	public void setSuccess(int success) {
-		this.success = success;
-	}
 	
 	public String getAcknowledgement() {
 		return acknowledgement;
