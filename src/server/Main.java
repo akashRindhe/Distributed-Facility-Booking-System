dd652d426a88e70dc038a9f49ae7d4c4b8b49a49
Shuvam Nandi <shuvamnandi9@gmail.com>
2017-04-02 18:41:25 +0800

    Server accepts optional parameters to specify port number, atMostServer (boolean) and simulateErrors(boolean)

--- src/server/Main.java
+++ src/server/Main.java
@@ -2,11 +2,30 @@ package server;
 
 import java.io.IOException;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		int port = 4445;
-		final Server server = new Server(port, false, false);
+		boolean atMostServer = false;
+		boolean simulateErrors = false;
+		
+		OptionParser optParser = new OptionParser();
+		optParser.accepts("port").withRequiredArg().ofType(Integer.class);
+		optParser.accepts("atMost").withRequiredArg().ofType(String.class);
+		optParser.accepts("simulateErrors").withRequiredArg().ofType(String.class);
+		
+		OptionSet optSet = optParser.parse(args);
+		if (optSet.has("port"))
+			port = (int) optSet.valueOf("port");
+		if (optSet.has("atMost"))
+			atMostServer = Boolean.parseBoolean((String) optSet.valueOf("atMost"));
+		if (optSet.has("simulateErrors"))
+			simulateErrors = Boolean.parseBoolean((String) optSet.valueOf("simulateErrors"));
+		
+		final Server server = new Server(port, atMostServer, simulateErrors);
 		
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 		    public void run() {
